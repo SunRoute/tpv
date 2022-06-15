db83770556797312a55191eb3ab1e9cd154e8024
Your Name <you@example.com>
2022-06-15 19:13:50 +0200

    tickets

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
-    echo("Hola Mundo");
+    $tickets = ['2205290001', '2205290002', '2206150001'];
 
-    var_dump("Hola");
+    function generarTicket($tickets){
+       
+        $fecha = date("ymd");
+        $ultimo_ticket = end($tickets);
+        $posTicket = strpos($ultimo_ticket, $fecha);
+        
+        if($posTicket !== false){
+            echo $ultimo_ticket + 1;
+        }else{
+            echo $fecha."0001";
+        }
+
+    }
+
+    generarTicket($tickets);
 
 ?>
